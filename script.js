6af246a56c97d6c74b2232e85fcd3838682af0f9
FlorFleitas <flor1197@hotmail.com>
2024-11-06 20:36:30 -0300

    scroll down cta

--- script.js
+++ script.js
@@ -12,11 +12,10 @@ document.addEventListener('DOMContentLoaded', function () {
         hamburger.classList.toggle("active");
     });
 
-    // Close the menu when any link is clicked
     menuLinks.forEach(function (link) {
         link.addEventListener("click", function () {
-            menu.classList.remove("show");  // Close the menu
-            hamburger.classList.remove("active");  // Remove the active state from the hamburger
+            menu.classList.remove("show");  
+            hamburger.classList.remove("active");  
         });
     });
 
@@ -26,7 +25,7 @@ document.addEventListener('DOMContentLoaded', function () {
 // Logo slider functionality
 const sliderItems = document.querySelector(".slider-items");
 let scrollAmount = 0;
-const scrollStep = 0.5; // Adjust speed here
+const scrollStep = 0.5; 
 
 function startSlider() {
     const interval = setInterval(() => {
@@ -43,11 +42,8 @@ function startSlider() {
 document.addEventListener("DOMContentLoaded", startSlider);
 
 /* BACK TO TOP BUTTON */
-
-// Obtener el botón
 const backToTopBtn = document.getElementById('backToTopBtn');
 
-// Mostrar el botón al desplazarse más de 100px
 window.onscroll = function () {
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
         backToTopBtn.classList.add('show');
@@ -55,8 +51,18 @@ window.onscroll = function () {
         backToTopBtn.classList.remove('show');
     }
 };
-
-// Desplazarse hacia arriba al hacer clic en el botón
 backToTopBtn.addEventListener('click', function () {
     window.scrollTo({ top: 0, behavior: 'smooth' });
 });
+
+/* SCROLL DOWN ARROW - HOME */
+document.querySelector('.scroll-link').addEventListener('click', function(e) {
+    e.preventDefault();  
+    
+    const targetId = this.getAttribute('href');
+    const targetElement = document.querySelector(targetId);
+    
+    targetElement.scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
